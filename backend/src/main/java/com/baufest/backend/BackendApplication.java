707397ef722167e76fd06c12aee14e3cafda7c5f
Alejandro Matias Sanchez <amsanchez@baufest.com>
2018-12-21 15:15:00 -0300

    refactor main app

--- backend/src/main/java/com/baufest/backend/BackendApplication.java
+++ backend/src/main/java/com/baufest/backend/BackendApplication.java
@@ -1,14 +1,26 @@
 package com.baufest.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendApplication.class);
+	}
 }
 
