04272f348d09b532d13374a68cc85ff933f2e71b
Alejandro Matias Sanchez <amsanchez@baufest.com>
2018-12-26 11:23:10 -0300

    annotations

--- app1/src/main/java/com/baufest/app1/App1Application.java
+++ app1/src/main/java/com/baufest/app1/App1Application.java
@@ -4,12 +4,18 @@ import com.baufest.app1.model.SimpleBean;
 import com.baufest.app1.repository.SimpleBeanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class App1Application extends SpringBootServletInitializer implements ApplicationListener<ContextRefreshedEvent> {
 
